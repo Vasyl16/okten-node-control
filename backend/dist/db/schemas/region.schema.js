31693bd3a26a394f83d022c5820f040cd3b85a82
Yurchuk Vasyl <mira07081984@gmail.com>
2026-03-19 16:11:32 +0200

    add .gitignore files

--- backend/dist/db/schemas/region.schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.regions = void 0;
-const pg_core_1 = require("drizzle-orm/pg-core");
-exports.regions = (0, pg_core_1.pgTable)('regions', {
-    id: (0, pg_core_1.serial)('id').primaryKey(),
-    name: (0, pg_core_1.varchar)('name', { length: 100 }).notNull(), // e.g. "Kyiv", "Lviv", "Odesa"
-    slug: (0, pg_core_1.varchar)('slug', { length: 100 }).notNull().unique(), // e.g. "kyiv", "lviv"
-});
